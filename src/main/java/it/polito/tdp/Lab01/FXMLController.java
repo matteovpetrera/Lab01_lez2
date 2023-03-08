4b388913ff6294c9cbd296c7243ffab37e932dde
matteopetrera <matteopetrera@localhost>
2023-03-08 17:54:10 +0100

    fine esercizio, funziona.

--- src/main/java/it/polito/tdp/Lab01/FXMLController.java
+++ src/main/java/it/polito/tdp/Lab01/FXMLController.java
@@ -1,6 +1,8 @@
 package it.polito.tdp.Lab01;
 
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.Lab01.model.Parole;
@@ -34,14 +36,36 @@ public class FXMLController {
 
     @FXML
     void doInsert(ActionEvent event) {
-
+    	String output = null;
+    	String parola = this.txtParola.getText();
+    	elenco.addParola(parola);
+    	List<String> listaParole = elenco.getElenco();
+    	Collections.sort(listaParole);
+    	output = daListaAString(listaParole);
+    	this.txtResult.setText(output);
     }
 
     @FXML
     void doReset(ActionEvent event) {
-
+    	
+    	elenco.reset();
+    	this.txtResult.setText("");
+    	
     }
-
+    
+    public String daListaAString(List<String> listaParole) {
+    	String output = null;
+    	for(String p: listaParole) {
+    		if(output==null) {
+    			output = p;
+    		}
+    		else if (output!=null){
+    			output = output+"\n"+p;
+    		}
+    	}
+    	return output;
+    }
+    
     @FXML
     void initialize() {
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
--- src/main/java/it/polito/tdp/Lab01/model/Parole.java
+++ src/main/java/it/polito/tdp/Lab01/model/Parole.java
@@ -1,24 +1,30 @@
 package it.polito.tdp.Lab01.model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
-		
+	
+	List<String> elenco;
+	
 	public Parole() {
 		//TODO
+		elenco = new LinkedList<String>();
 	}
 	
 	public void addParola(String p) {
+		elenco.add(p);
 		//TODO
 	}
 	
 	public List<String> getElenco() {
 		//TODO
-		return null;
+		return elenco;
 	}
 	
 	public void reset() {
 		// TODO
+		elenco.removeAll(elenco);
 	}
 
 }
